d2ce5d9955b7c0de232bf8760eaa59e211e7e437
fluff663 <fluff663@fluffgames.com>
2023-04-24 18:43:58 -0400

    Previous change broke things

--- main.py
+++ main.py
@@ -34,57 +34,57 @@ winsound.Beep(18000, 1000)  # Beep at 1000 Hz for 100 ms
 f18000hz = input("Did You hear the tone? Y/N")
 winsound.Beep(20000, 1000)  # Beep at 1000 Hz for 100 ms
 f20000hz = input("Did You hear the tone? Y/N")
-if userpref == "y" or "Y":
+if userpref == "y":
     buserpref = True
-if userpref == "n" or "N":
+if userpref == "n":
     buserpref = False
-if f500hz == "y" or "Y":
+if f500hz == "y" :
     b500hz = True
-if f500hz == "n" or "N":
+if f500hz == "n" :
     b500hz = False
-if f1000hz == "y" or "Y":
+if f1000hz == "y" :
     b1000hz = True
-if f1000hz == "n" or "N":
+if f1000hz == "n" :
     b1000hz = False
-if f2000hz == "y" or "Y":
+if f2000hz == "y" :
     b2000hz = True
-if f2000hz == "n" or "N":
+if f2000hz == "n" :
     b2000hz = False
-if f4000hz == "y" or "Y":
+if f4000hz == "y" :
     b4000hz = True
-if f4000hz == "n" or "N":
+if f4000hz == "n" :
     b4000hz = False
-if f6000hz == "y" or "Y":
+if f6000hz == "y" :
     b6000hz = True
-if f6000hz == "n" or "N":
+if f6000hz == "n" :
     b6000hz = False
-if f8000hz == "y" or "Y":
+if f8000hz == "y" :
     b8000hz = True
-if f8000hz == "n" or "N":
+if f8000hz == "n" :
     b8000hz = False
-if f10000hz == "y" or "Y":
+if f10000hz == "y" :
     b10000hz = True
-if f10000hz == "n" or "N":
+if f10000hz == "n" :
     b10000hz = False
-if f12000hz == "y" or "Y":
+if f12000hz == "y" :
     b12000hz = True
-if f12000hz == "n" or "N":
+if f12000hz == "n" :
     b12000hz = False
-if f14000hz == "y" or "Y":
+if f14000hz == "y" :
     b14000hz = True
-if f14000hz == "n" or "N":
+if f14000hz == "n" :
     b14000hz = False
-if f16000hz == "y" or "Y":
+if f16000hz == "y" :
     b16000hz = True
-if f16000hz == "n" or "N":
+if f16000hz == "n" :
     b16000hz = False
-if f18000hz == "y" or "Y":
+if f18000hz == "y" :
     b18000hz = True
-if f18000hz == "n" or "N":
+if f18000hz == "n" :
     b18000hz = False
-if f20000hz == "y" or "Y":
+if f20000hz == "y" :
     b20000hz = True
-if f20000hz == "n" or "N":
+if f20000hz == "n" :
     b20000hz = False
 try:
     buserpref
